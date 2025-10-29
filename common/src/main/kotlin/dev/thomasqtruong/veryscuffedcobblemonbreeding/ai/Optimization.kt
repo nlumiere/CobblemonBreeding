f620ca7ec63cb9b57c5955815f91795eb3e0ae41
nlumiere <nicholaslumiere@gmail.com>
2025-10-28 22:13:37 -0700

    fixes crafting

---
+++ common/src/main/kotlin/dev/thomasqtruong/veryscuffedcobblemonbreeding/ai/Optimization.kt
@@ -0,0 +1,4 @@
+package dev.thomasqtruong.veryscuffedcobblemonbreeding.ai
+
+class Optimization {
+}
---
+++ common/src/main/kotlin/dev/thomasqtruong/veryscuffedcobblemonbreeding/ai/VillagerActor.kt
@@ -0,0 +1,4 @@
+package dev.thomasqtruong.veryscuffedcobblemonbreeding.ai
+
+class VillagerActor {
+}
